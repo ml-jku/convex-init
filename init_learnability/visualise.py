15174b849bab450513070a48a8e299bd36bc4158
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2023-02-10 13:49:11 +0100

    fine-tune learnability plots

--- init_learnability/visualise.py
+++ init_learnability/visualise.py
@@ -32,28 +32,28 @@ def visualise_results(data: dict[tuple[str, int, str, bool], np.ndarray],
     dataset_options = tuple(sorted({k[0] for k in data.keys()}, key=len))
     depth_options = tuple(sorted({k[1] for k in data.keys()}))
     nrows, ncols = len(depth_options), len(dataset_options)
-    fig = plt.figure(figsize=(ncols * 6.4, nrows * 4.8))
+    fig = plt.figure(figsize=(ncols * 5.6, nrows * 4.2))
     axes = fig.subplots(nrows, ncols, squeeze=False)
     for ax in axes.flat:
-        axins = ax.inset_axes([.5, .3, .45, .35])
+        axins = ax.inset_axes([.5, .4, .45, .35])
         axins.get_xaxis().set_visible(False)
         if scale is not None:
             ax.set_yscale(scale)
             axins.set_yscale(scale)
 
     label_colors = {
-        ("", True): ("non-convex", "gray"),
-        ("clip", False): ("clip raw", plt.cm.Paired(0)),
-        ("clip", True): ("clip init", "white"),
-        ("exp", False): ("exp raw", plt.cm.Paired(2)),
-        ("exp", True): ("exp init", plt.cm.Paired(3)),
-        ("icnn", False): ("icnn raw", plt.cm.Paired(4)),
-        ("icnn", True): ("icnn init", plt.cm.Paired(5)),
+        ("", True): ("non-convex", "lightgray"),
+        ("icnn", True): ("ICNN ours", plt.cm.tab20(0)),
+        ("icnn", False): ("ICNN He", plt.cm.tab20(1)),
+        ("exp", True): ("exp-ICNN ours", plt.cm.tab20(2)),
+        ("exp", False): ("exp-ICNN He", plt.cm.tab20(3)),
+        ("clip", True): ("clip-ICNN ours", plt.cm.tab20(4)),
+        ("clip", False): ("clip-ICNN He", plt.cm.tab20(5)),
     }
 
     for k, v in data.items():
         dataset_name, num_hidden, positivity, best_init = k
-        if positivity == "clip" and best_init is True:
+        if positivity == "clip":
             continue
         ax = axes[
             depth_options.index(num_hidden),
@@ -67,31 +67,36 @@ def visualise_results(data: dict[tuple[str, int, str, bool], np.ndarray],
         ax.fill_between(x, q1, q3, color=col, alpha=.5, zorder=1)
         ax.plot(q2, color=col, label=lbl, zorder=3)
         ax.plot(x, q0, "--", q4, "--", color=col, zorder=2)
+        if positivity == "":
+            ax.set_ylim(None, 1.05 * q4.max())
+        elif ax.get_ylim()[0] > q0.min():
+            ax.set_ylim(0.95 * q0.min(), None)
 
         # zoomed
         axins = ax.child_axes[0]
         axins.fill_between(x, q1, q3, color=col, alpha=.5, zorder=1)
         axins.plot(q2, color=col, label=lbl, zorder=3)
         axins.plot(x, q0, "--", q4, "--", color=col, zorder=2)
-        if positivity == "":
-        #     axins.set_xlim(0, v.shape[1] - 1)
-        #     axins.set_ylim(.75 * q0.max(), None)
-        # elif axins.get_ylim()[1] < q4.max():
-        #     axins.set_ylim(None, q4.max())
-            axins.set_ylim(None, .95 * q4.max())
-        elif axins.get_ylim()[0] > q0.min():
-            axins.set_ylim(q0.min(), None)
+        # if positivity == "":
+        # #     axins.set_xlim(0, v.shape[1] - 1)
+        # #     axins.set_ylim(.75 * q0.max(), None)
+        # # elif axins.get_ylim()[1] < q4.max():
+        # #     axins.set_ylim(None, q4.max())
+        #     axins.set_ylim(None, .95 * q4.max())
+        # elif axins.get_ylim()[0] > q0.min():
+        #     axins.set_ylim(q0.min(), None)
 
     for ax in axes.flat:
         axins = ax.child_axes[0]
         ax.indicate_inset_zoom(axins)
 
-    axes[0, 0].legend()
+    axes[0, 1].legend(loc="lower left")
     for ax, col_title in zip(axes[0, :], dataset_options):
         ax.set_title(col_title)
-    for ax, depth in zip(axes[:, 0], depth_options):
-        ax.text(-.1, .5, f"{depth + 1}-layer net", transform=ax.transAxes,
-                ha="right", va="center", rotation="vertical")
+    # for ax, depth in zip(axes[:, 0], depth_options):
+    #     ax.text(-.1, .5, f"{depth + 1}-layer net", transform=ax.transAxes,
+    #             ha="right", va="center", rotation="vertical")
+    fig.tight_layout()
     return fig
 
 
