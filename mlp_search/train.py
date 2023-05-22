2f7e1446a6449b2d7fd07010936bccfe63f76141
Pieter-Jan Hoedt <hoedt@ml.jku.at>
2023-05-22 10:05:23 +0200

    include final train and visualisation code

---
+++ mlp_search/train.py
@@ -0,0 +1,129 @@
+import random
+import time
+from pathlib import Path
+
+import torch
+import upsilonconf
+from torch import nn
+from torch.utils.data import random_split, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torchvision import datasets
+from torchvision.transforms import transforms
+from upsilonconf import save_config
+
+from mlp_search.search import get_model
+from pre_processing import Whiten
+from trainer import Trainer
+from utils import make_deterministic, lecun_init_, he_init_
+
+
+def get_data(name: str, root: str, pre_process: str = "normal"):
+    """
+    Create train- and valid dataset for experiment.
+
+    Parameters
+    ----------
+    name : str
+        The name of the dataset to use.
+    root : str
+        Path to where the data is stored on disk.
+    train_split : float, optional
+        The fraction of data to use for training.
+
+    Returns
+    -------
+    (train, valid) : tuple of two Datasets
+        A tuple of datasets for updating and evaluating the network.
+    (in_shape, class_count) : tuple of torch.Size and int
+        A tuple with the input shape and number of classes for this dataset.
+    """
+    name = name.lower().strip()
+    pre_process = pre_process.lower().strip()
+
+    if name == "mnist":
+        dataset = datasets.MNIST
+        mean, std = (0.1307, ), (0.3081, )
+        shapes = torch.Size((1, 28, 28)), 10
+    elif name == "cifar10":
+        dataset = datasets.CIFAR10
+        mean, std = (0.4914, 0.4822, 0.4465), (0.2470, .2435, 0.2616)
+        shapes = torch.Size((3, 32, 32)), 10
+    elif name == "cifar100":
+        dataset = datasets.CIFAR100
+        mean, std = (0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)
+        shapes = torch.Size((3, 32, 32)), 100
+    else:
+        raise ValueError(f"unknown dataset '{name}'")
+
+    pre_processing = transforms.Compose([transforms.ToTensor()])
+    train_data = dataset(root, train=True, transform=pre_processing)
+    test_data = dataset(root, train=False, transform=pre_processing)
+    if pre_process == "normal":
+        pre_processing.transforms.append(transforms.Normalize(mean, std))
+    elif pre_process == "pca":
+        pre_processing.transforms.append(Whiten(train_data, zca=False))
+    elif pre_process == "zca":
+        pre_processing.transforms.append(Whiten(train_data, zca=True))
+
+    return (train_data, test_data), shapes
+
+
+def run(hparams, sys_config, log_dir):
+    make_deterministic(hparams.seed)
+
+    (train, test), shapes = get_data(root=sys_config.data_root, **hparams.data)
+    train_loader = DataLoader(train, shuffle=True, batch_size=hparams.batch_size,
+                              num_workers=sys_config.num_workers)
+    test_loader = DataLoader(test, batch_size=len(test),
+                              num_workers=sys_config.num_workers)
+
+    device = sys_config.device
+    if device is None:
+        device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = get_model(*shapes, **hparams.model).to(device)
+
+    trainer = Trainer(
+        model=model,
+        objective=nn.CrossEntropyLoss(reduction="sum"),
+        optimiser=torch.optim.Adam(model.parameters(), **hparams.adam),
+        tb_writer=SummaryWriter(log_dir),
+    )
+
+    # logging
+    save_config(hparams, f"{log_dir}/config.yaml")
+
+    results = trainer.train(train_loader, test_loader, hparams.num_epochs)
+    trainer.logger.close()
+    return results["acc"]
+
+
+if __name__ == "__main__":
+    from concurrent.futures import ProcessPoolExecutor
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument("--data-root", type=str, default="~/.pytorch")
+    parser.add_argument("--device", type=str, choices=["cpu", "cuda"], default=None)
+    parser.add_argument("--num-workers", type=int, default=1)
+    parser.add_argument("--parallel", "-P", type=int, default=5)
+    hparams, sys_config = upsilonconf.from_cli(parser=parser)
+    repetitions = 10
+
+    run_id = hparams.pop("id")
+    stamp = time.strftime("%y%j-%H%M%S")
+
+    def _run(hpars):
+        log_path = Path("results", "mlp_search", "_".join([hpars.id, stamp]))
+        try:
+            return run(hpars, sys_config, log_path), log_path
+        except RuntimeError as err:
+            print(f"run {hpars.id} failed:", err)
+            return float("nan"), None
+
+    rng = random.Random(hparams.pop("seed"))
+    seeds = [rng.randint(10_000, 100_000) for _ in range(repetitions)]
+    pool = ProcessPoolExecutor(max_workers=sys_config.parallel)
+    accuracies = pool.map(_run, [
+        hparams | {"seed": s, "id": ".".join([run_id, str(i)])}
+        for i, s in enumerate(seeds)])
+    print([acc for acc, _ in accuracies])
+    pool.shutdown()
---
+++ mlp_search/visualise.py
@@ -0,0 +1,164 @@
+from pathlib import Path
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def retrieve_five_number_summary(path: Path, pattern: str, tb_tag: str = "train/avg_loss"):
+    from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+
+    results = []
+    for p in path.glob(pattern):
+        if p.is_dir():
+            event_path = next(p.glob(f"events.out.tfevents.*"))
+            ea = EventAccumulator(str(event_path))
+            events = ea.Reload().Scalars(tb_tag)
+            results.append([e.value for e in events])
+
+    return np.quantile(results, q=[0., .25, .5, .75, 1.], axis=0)
+
+
+def plot_five_number_summary(summary, label=None, color=None, ax=None):
+    if len(summary) != 5:
+        raise ValueError("not a five-number summary")
+    if ax is None:
+        ax = plt.gca()
+    if color is None:
+        color = next(ax._get_lines.prop_cycler)["color"]
+
+    # lines = ax.semilogy(summary[[0, -1]].T, "--", color=color)
+    # color = lines[0].get_color()
+    ax.fill_between(range(summary.shape[1]), summary[1], summary[-2],
+                    alpha=0.5, color=color)
+    ax.semilogy(summary[2], color=color, label=label)
+    return ax
+
+
+def generate_mnist_plot():
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8.4, 4.2))
+    result_path = Path("results", "mlp_search")
+
+    # non-convex:      mnist071
+    # exp-icnn:        convex1_mnist023
+    # exp-icnn + init: convex_mnist066
+    # icnn:            icnn1_mnist0?? (icnn_mnist047)
+    # icnn + init:     icnn2_mnist052
+
+    stats = retrieve_five_number_summary(result_path, "mnist???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "non-convex", color="gray", ax=ax1)
+    # stats = retrieve_five_number_summary(result_path, "convex1_mnist_rep?_*", tb_tag="valid/loss")
+    # plot_five_number_summary(stats, "exp-ICNN", color=plt.cm.tab20(3), ax=ax1)
+    # stats = retrieve_five_number_summary(result_path, "convex_mnist_rep?_*", tb_tag="valid/loss")
+    # plot_five_number_summary(stats, "exp-ICNN + init", color=plt.cm.tab20(2), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "icnn1_mnist???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN", color=plt.cm.tab10(2), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "skip_mnist???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN + skip", color=plt.cm.tab10(1), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "icnn2_mnist???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN + init", color="#0084bb", ax=ax1)
+    ax1.set_ylim(None, 1.)
+    ax1.set_ylabel("average loss")
+    ax1.legend()
+
+    stats = retrieve_five_number_summary(result_path, "mnist???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "non-convex", color="gray", ax=ax2)
+    # stats = retrieve_five_number_summary(result_path, "convex1_mnist_rep?_*", tb_tag="valid/acc")
+    # plot_five_number_summary(stats, "exp-ICNN", color=plt.cm.tab20(3), ax=ax2)
+    # stats = retrieve_five_number_summary(result_path, "convex_mnist_rep?_*", tb_tag="valid/acc")
+    # plot_five_number_summary(stats, "exp-ICNN + init", color=plt.cm.tab20(2), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "icnn1_mnist???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN", color=plt.cm.tab10(2), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "skip_mnist???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN + skip", color=plt.cm.tab10(1), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "icnn2_mnist???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN + init", color="#0084bb", ax=ax2)
+    ax2.yaxis.tick_right()
+    ax2.yaxis.set_label_position("right")
+    ax2.set_yscale("linear")
+    ax2.set_ylim(0.9, 1.0)
+    ax2.set_ylabel("accuracy")
+
+    # ax1.set_title("baseline hyper-parameters")
+    # base = retrieve_five_number_summary(result_path, "mnist071?_*")
+    # plot_five_number_summary(base, "baseline", ax=ax1)
+    # icnn = retrieve_five_number_summary(result_path, "mnist071icnn?_*")
+    # plot_five_number_summary(icnn, "ICNN", ax=ax1)
+    # conv = retrieve_five_number_summary(result_path, "mnist071convex?_*")
+    # plot_five_number_summary(conv, "convex", ax=ax1)
+    # ax1.legend()
+
+    # ax2.set_title("ICNN hyper-parameters")
+    # base = retrieve_five_number_summary(result_path, "icnn_mnist002base?_*")
+    # plot_five_number_summary(base, "baseline", ax=ax2)
+    # icnn = retrieve_five_number_summary(result_path, "icnn_mnist002?_*")
+    # plot_five_number_summary(icnn, "ICNN", ax=ax2)
+    # conv = retrieve_five_number_summary(result_path, "icnn_mnist002convex?_*")
+    # plot_five_number_summary(conv, "convex", ax=ax2)
+
+    # ax3.set_title("convex hyper-parameters")
+    # base = retrieve_five_number_summary(result_path, "convex_mnist066base?_*")
+    # plot_five_number_summary(base, "baseline", ax=ax3)
+    # icnn = retrieve_five_number_summary(result_path, "convex_mnist066icnn?_*")
+    # plot_five_number_summary(icnn, "ICNN", ax=ax3)
+    # conv = retrieve_five_number_summary(result_path, "convex_mnist066?_*")
+    # plot_five_number_summary(conv, "convex", ax=ax3)
+
+    fig.tight_layout()
+    return fig
+
+
+def generate_cifar_plot():
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8.4, 4.2))
+    result_path = Path("results", "mlp_search")
+
+    # non-convex:      cifar107
+    # exp-icnn:        convex1_cifar023
+    # exp-icnn + init: convex_cifar044
+    # icnn:            icnn1_cifar023 (icnn_cifar022)
+    # icnn + init:     icnn2_cifar022
+
+    stats = retrieve_five_number_summary(result_path, "cifar???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "non-convex", color="gray", ax=ax1)
+    # stats = retrieve_five_number_summary(result_path, "convex1_cifar_rep?_*", tb_tag="valid/loss")
+    # plot_five_number_summary(stats, "exp-ICNN", color=plt.cm.tab20(3), ax=ax1)
+    # stats = retrieve_five_number_summary(result_path, "convex_cifar_rep?_*", tb_tag="valid/loss")
+    # plot_five_number_summary(stats, "exp-ICNN + init", color=plt.cm.tab20(2), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "icnn1_cifar???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN", color=plt.cm.tab10(2), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "skip_cifar???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN + skip", color=plt.cm.tab10(1), ax=ax1)
+    stats = retrieve_five_number_summary(result_path, "icnn2_cifar???.?_*", tb_tag="valid/loss")
+    plot_five_number_summary(stats, "ICNN + init", color="#0084bb", ax=ax1)
+    ax1.set_ylim(None, 6.)
+    ax1.set_ylabel("average loss")
+    ax1.legend()
+
+    stats = retrieve_five_number_summary(result_path, "cifar???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "non-convex", color="gray", ax=ax2)
+    # stats = retrieve_five_number_summary(result_path, "convex1_cifar_rep?_*", tb_tag="valid/acc")
+    # plot_five_number_summary(stats, "exp-ICNN", color=plt.cm.tab20(3), ax=ax2)
+    # stats = retrieve_five_number_summary(result_path, "convex_cifar_rep?_*", tb_tag="valid/acc")
+    # plot_five_number_summary(stats, "exp-ICNN + init", color=plt.cm.tab20(2), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "icnn1_cifar???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN", color=plt.cm.tab10(2), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "skip_cifar???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN", color=plt.cm.tab10(1), ax=ax2)
+    stats = retrieve_five_number_summary(result_path, "icnn2_cifar???.?_*", tb_tag="valid/acc")
+    plot_five_number_summary(stats, "ICNN + init", color="#0084bb", ax=ax2)
+    ax2.yaxis.tick_right()
+    ax2.yaxis.set_label_position("right")
+    ax2.set_yscale("linear")
+    ax2.set_ylim(0.3, 0.6)
+    ax2.set_ylabel("accuracy")
+
+    # ax1.set_title("baseline hyper-parameters")
+    # base = retrieve_five_number_summary(result_path, "cifar_base_rep?_*")
+    # plot_five_number_summary(base, "baseline", ax=ax1)
+    # icnn = retrieve_five_number_summary(result_path, "cifar_icnn_rep?_*")
+    # plot_five_number_summary(icnn, "ICNN", ax=ax1)
+    # conv = retrieve_five_number_summary(result_path, "cifar_conv_rep?_*")
+    # plot_five_number_summary(conv, "convex", ax=ax1)
+    # ax1.legend()
+
+    fig.tight_layout()
+    return fig
